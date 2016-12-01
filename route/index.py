18d0d3d7f2d02b5e24c22fe3ba2d51e40c09b6ea
weichensun <weichensun@kkbox.com>
2016-12-01 22:01:24 +0800

    Add one more space

--- route/index.py
+++ route/index.py
@@ -82,7 +82,7 @@ class API_Worker( worker_base.API_Worker_Base ):
             send_slack_message( "4 Spaces!!!!" )
         elif data_text == '+2':
             send_slack_message( "醒醒吧,你只有一個人" )
-       elif data_text == '+3':
+        elif data_text == '+3':
             send_slack_message( "3333" )
 
         self.reply( '', 'text/html', 200 )
